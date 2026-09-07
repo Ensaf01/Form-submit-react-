50e0d3489a166d29de78706330472f81e7c7dc50
ensaf01 <mohammedensaf402@gmail.com>
2026-09-07 11:56:19 +0600

    2nd way to submit form and reuired filed and show erroe message

--- src/components/StateFullForm/StateFullForm.jsx
+++ src/components/StateFullForm/StateFullForm.jsx
@@ -2,38 +2,51 @@ import { useState } from "react";
 
 
 const StateFullForm = () => {
-    const [name,setName] =useState(null)
-     const [emails,setemail] =useState(null)
+    const [name, setName] = useState(null)
+    const [emails, setemail] = useState(null)
+    const [error, setError] = useState()
 
-    const handleSubmit=e=>{
+    const handleSubmit = e => {
         e.preventDefault();
-        console.log("submited :", name,emails);
+        if (name.length < 4) {
+            setError('name length must be gather then 5')
+        }
+        else {
+            setError('');
+            console.log("submited :", name, emails);
+        }
+
     }
-     const handleNameChange= x =>{
+    const handleNameChange = x => {
         console.log(x.target.value)
         setName(x.target.value);
-     }
+    }
 
-     const handleEmailChange =e =>{
-         console.log(e.target.value)
+    const handleEmailChange = e => {
+        console.log(e.target.value)
         setemail(e.target.value);
-     }
+    }
 
 
 
     return (
         <div>
-             <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit}>
                 <input onChange={handleNameChange} type="text" name="names" id="" />
                 <br />
                 <input onChange={handleEmailChange} type="email" name="email" id="" />
                 <br />
-                <input type="password" name="password" />
-                 <br />
+                <input type="password" name="password" required />
+                <br />
 
                 <input type="submit" value="submit" />
+
+                {/* // error sow */}
+                {
+                    error && <p>{error}</p>
+                }
             </form>
-            
+
         </div>
     );
 };
